172bd0ba362b078b54d786f0660be591a96ffea8
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2019-06-10 22:13:31 +1000

    Final commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Get your sleeping bag ready because CampJS is coming soon.</p>
+  </Layout>
+)
+
+export default IndexPage
